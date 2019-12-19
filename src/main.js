e06c36b42297a15d3c4151ffc08ad87186e93364
Lonzo <1318947374@qq.com>
2019-12-20 00:05:51 +0800

    下一步拆组件

--- src/main.js
+++ src/main.js
@@ -1,23 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import http from '@/api'
-import "@/mock"
-import '@/common/stylus/style.styl'
-import '@/common/stylus/transition.stylus'
+// import router from './router'
+// import http from '@/api'
 
 // import axios from 'axios'
 Vue.config.productionTip = false;
-Vue.prototype.$http = http
-Vue.prototype.bus = new Vue()
-// (async()=>{
-//   const data = axios.get("/api/sellers")
-//   console.log(data.data);
-  
-// })()
+// Vue.prototype.$http = http
+// Vue.prototype.bus = new Vue()
+
 new Vue({
   render: h => h(App),
-  router
 }).$mount('#app');
 
 
